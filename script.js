8aab3d6f77d1bb036c9a02592fa96dd42962c94a
sarvdavoudi <sarvdavoudi3000@yahoo.com>
2023-03-08 17:42:26 +0330

    version1

---
+++ script.js
@@ -0,0 +1,24 @@
+const fetchCoinsFromApi = async () => {
+  const Response = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+  );
+  const jsonResponse = await Response.json();
+  return jsonResponse;
+};
+
+//when page is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  let timer = setInterval(async() => {
+  const coins = await fetchCoinsFromApi();
+  showData(coins);
+  }, 5000);
+});
+
+
+
+
+const showData = (arrayOfCoins) => {
+  const finalView = arrayOfCoins.map((item) => `<p>${item.name} : ${item.current_price} </p>`);
+  showbox = document.getElementById("data-box");
+  showbox.innerHTML = finalView.join("");
+};
